a496d1d950b090f2828c8ec0e59ac716dce4325f
SorinPusderca <pusderca.sorin@gmail.com>
2022-08-01 08:40:58 +0300

    update for home, pages, table, customHooks

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,28 +1,39 @@
-import { useEffectOnce } from '../customHooks/customHooks';
+import { useState } from "react";
+import { useEffectOnce } from "../customHooks/customHooks";
 import { getCoinsMarkets } from "../services/api";
-import { useState } from 'react';
+import Table from "../components/table/table";
+
 
 const Home = () => {
+  const [coins, setCoins] = useState([]);
 
-    const [coins, setCoins]=useState()
+  useEffectOnce(() => {
+    async function getData() {
+      const res = await getCoinsMarkets();
+      if (res.status === 200) {
+        const filteredData = res.data.map((obj) => {
+          return {
+            image: { value: obj.image, type: "image"},
+            name: { value: obj.name, type: "string"},
+            symbol: { value: obj.symbol, type: "string"},
+            current_price: { value: obj.current_price, type: "string"},
+            high_24_hour_price: { value: obj.high_24h, type: "string"},
+            low_24_hour_price: { value: obj.low_24h, type: "string"}
+          };
+        });
+        setCoins(filteredData);
+      } else {
+        console.log(res);
+      }
+    }
+    getData();
+  });
 
-    useEffectOnce(() => {
-        async function getData() {
-            const res = await getCoinsMarkets();
-            if(res.status===200){
-                setCoins(res.data)
-            }
-            else{
-                console.log(res);
-            }
-            console.log(res);
-        }
-        getData();
-    })
-
-    return(
-        <div>Home</div>
-    )
-}
+  return (
+    <div>
+      {coins.length > 0 && <Table data={coins} />}
+    </div>
+  );
+};
 
 export default Home;
---
+++ src/services/tool.js
@@ -0,0 +1,8 @@
+const arr=['unu', 'doi', 'trei'];
+const key= 'blalalal';
+const obj={};
+
+obj[key]='test'
+
+console.log(obj)
+
